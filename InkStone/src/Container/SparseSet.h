475eeb817251dbad64351e5c6260da99be43840a
Nofer Xue <nortusx@gmail.com>
2026-03-08 21:10:30 -0400

    Update SparseSet.h

--- InkStone/src/Container/SparseSet.h
+++ InkStone/src/Container/SparseSet.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <utility>
@@ -9,39 +10,70 @@ namespace NXTN {
 	class SparseSet
 	{
 	public:
-		SparseSet() {}
-		virtual ~SparseSet() {}
+		SparseSet() = default;
+		~SparseSet() = default;
 
-		constexpr size_t Size() const {
+		size_t Size() const noexcept {
 			return m_DenseKey.size();
 		}
 
 		bool Has(uint32_t key) const {
-			if (key < m_Sparse.size()) {
-				return m_Sparse[key] < SIZE_MAX;
-			}
-			return false;
+			return key < m_Sparse.size()
+				&& m_Sparse[key] < m_DenseKey.size()
+				&& m_DenseKey[m_Sparse[key]] == key;
+		}
+
+		T& Get(uint32_t key) {
+			assert(Has(key) && "[Sparse Set] Item does not exist");
+
+			return m_DenseData[m_Sparse[key]];
+		}
+
+		const T& Get(uint32_t key) const {
+			assert(Has(key) && "[Sparse Set] Item does not exist");
+
+			return m_DenseData[m_Sparse[key]];
 		}
 
 		bool Add(uint32_t key) {
+
 			if (Has(key)) {
 				return false;
 			}
-			if (key < m_Sparse.size()) {
-				m_DenseKey.push_back(key);
-				m_DenseData.emplace_back();
-				m_Sparse[key] = m_DenseKey.size() - 1;
+			if (key >= m_Sparse.size()) {
+				m_Sparse.resize(static_cast<size_t>(key) + 1, SIZE_MAX);
 			}
+			m_Sparse[key] = m_DenseKey.size();
+			m_DenseKey.push_back(key);
+			m_DenseData.emplace_back();
+
+			return true;
+		}
+
+		void Update(uint32_t key, const T& data) {
+			if (key < m_Sparse.size()) {
+				// Add
+				if (m_Sparse[key] == SIZE_MAX) {
+					m_DenseKey.push_back(key);
+					m_DenseData.push_back(data);
+					m_Sparse[key] = m_DenseKey.size() - 1;
+				}
+				// Update
+				else {
+					m_DenseData[m_Sparse[key]] = data;
+				}
+			}
+			// Add
 			else {
 				m_Sparse.resize(static_cast<size_t>(key) + 1, SIZE_MAX);
 
 				m_DenseKey.push_back(key);
-				m_DenseData.emplace_back();
+				m_DenseData.push_back(data);
 				m_Sparse[key] = m_DenseKey.size() - 1;
 			}
 		}
 
-		void Update(uint32_t key, T& data) {
+		void Update(uint32_t key, T&& data) {
 			if (key < m_Sparse.size()) {
 				// Add
 				if (m_Sparse[key] == SIZE_MAX) {
@@ -65,22 +97,35 @@ namespace NXTN {
 		}
 
 		bool Remove(uint32_t key) {
-			if (Has(key)) {
-				size_t last = m_DenseData.size() - 1;
-
-				m_Sparse[m_DenseKey[last]] = m_Sparse[key];
-
-				m_DenseKey[m_Sparse[key]] = m_DenseKey[last];
-				m_DenseKey.pop_back();
-
-				m_DenseData[m_Sparse[key]] = std::move(m_DenseData[last]);
-				m_DenseData.pop_back();
-
-				m_Sparse[key] = SIZE_MAX;
-
-				return true;
+			if (!Has(key)) {
+				return false;
 			}
-			return false;
+
+			size_t denseIdxLast = m_DenseData.size() - 1;
+			size_t denseIdxToRmv = m_Sparse[key];
+
+			if (denseIdxToRmv != denseIdxLast) {
+				// The sparse for the last item in dense now maps to the one to be removed/overwritten
+				m_Sparse[m_DenseKey[denseIdxLast]] = denseIdxToRmv;
+
+				m_DenseKey[denseIdxToRmv] = m_DenseKey[denseIdxLast];
+				m_DenseData[denseIdxToRmv] = std::move(m_DenseData[denseIdxLast]);
+			}
+
+			m_DenseKey.pop_back();
+			m_DenseData.pop_back();
+
+			m_Sparse[key] = SIZE_MAX;
+
+			return true;
+		}
+
+		//T& Get(uint32_t key) {
+
+		//}
+
+		const std::vector<uint32_t>& Keys() const {
+			return m_DenseKey;
 		}
 
 	private:
